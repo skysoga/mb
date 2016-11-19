dde2fb8df46f30bc2fcbebc401bf6792312b5cc4
wzw <1007442838@qq.com>
2016-11-19 18:16:07 +0800

    修改因为头部 双选按钮的to中未做兼容产生的警告

--- src/components/title-info.vue
+++ src/components/title-info.vue
@@ -2,8 +2,8 @@
 	<header class="top">
     <span v-html="meta.title">
     </span>
-    <ul v-show="meta.titleList">
-    <router-link tag="li" v-for="i in meta.titleList" :to="i.to"><a>{{i.title}}</a><router-link>
+    <ul v-if="meta.titleList">
+    <router-link tag="li" v-for="i in meta.titleList" :to="i.to||''"><a>{{i.title}}</a><router-link>
     </ul>
     <router-link v-show="meta.link" :to="meta.link||''" class="iconfont back"></router-link>
     <router-link  v-show="meta.service" class="right" to="/service">客服</router-link>
