a82aa772b6069687dd3330aab27f549032418110
wzw <1007442838@qq.com>
2016-11-19 14:56:50 +0800

    允许本地设置title和link修改顶部信息

--- src/components/title-info.vue
+++ src/components/title-info.vue
@@ -1,11 +1,11 @@
 <template>
 	<header class="top">
-    <span v-html="this.$route.meta.title">
+    <span v-html="title||this.$route.meta.title">
     </span>
-    <ul v-show="this.$route.meta.titleList">
+    <ul v-show="title||this.$route.meta.titleList">
     <router-link tag="li" v-for="i in this.$route.meta.titleList" :to="i.to"><a>{{i.title}}</a><router-link>
     </ul>
-    <router-link v-show="this.$route.meta.link" :to="this.$route.meta.link||''" class="iconfont back"></router-link>
+    <router-link v-show="link||this.$route.meta.link" :to="link||this.$route.meta.link||''" class="iconfont back"></router-link>
     <router-link  v-show="this.$route.meta.service" class="right" to="/service">客服{{this.route}}</router-link>
   </header>
 </template>
