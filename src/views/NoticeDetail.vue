dda0c5d212f5899cd4403af71afb123d986a332f
明明明天 <1007503699@qq.com>
2016-11-21 14:11:22 +0800

    新增letter页面脚本以及优化playerhome

--- src/views/NoticeDetail.vue
+++ src/views/NoticeDetail.vue
@@ -10,24 +10,27 @@
 </template>
 
 <script type="text/javascript">
-  export default{
-    data(){
+  export default {
+    data() {
       return {
-        initData:[]
+        initData: []
       }
     },
-    created(){
-      _fetch({Action:"GetNoticeContent",ID:this.$route.params.ID}).then((res)=>{
-        if(res.ok){
-          res.json().then((data)=>{
-             if (data.Code===1) {
-               this.initData=data.BackData[0];
-             }else {
-               console.log("return error");
-             }
+    created() {
+      _fetch({
+        Action: "GetNoticeContent",
+        ID: this.$route.params.ID
+      }).then((res) => {
+        if (res.ok) {
+          res.json().then((data) => {
+            if (data.Code === 1) {
+              this.initData = data.BackData[0];
+            } else {
+              layer.msgWarn("return error");
+            }
           })
-        }else {
-          console.log("request error");
+        } else {
+          layer.msgWarn("request error");
         }
       })
     }
@@ -36,14 +39,50 @@
 
 <style lang="scss" scoped>
   @import '../scss/public.scss';
-  body{background:#fff}
-  article{width:15rem;margin:.5em auto;line-height:1em}
-  article h2{font-size:.9em;line-height:1.3}
-  article>span,article time{font-size:.7em}
-  article .hr1px{margin:.5em auto}
-  article>div{font-size:.8em;line-height:1.25em;font-weight:400}
-  article>div strong{font-style:normal;font-weight:400}
-  .cGold{color:#f46e00}
-  .cGray{color:#bababa}
-  .cGreen{color:#dc2e2e}
+  body {
+    background: #fff
+  }
+
+  article {
+    width: 15rem;
+    margin: .5em auto;
+    line-height: 1em
+  }
+
+  article h2 {
+    font-size: .9em;
+    line-height: 1.3
+  }
+
+  article>span,
+  article time {
+    font-size: .7em
+  }
+
+  article .hr1px {
+    margin: .5em auto
+  }
+
+  article>div {
+    font-size: .8em;
+    line-height: 1.25em;
+    font-weight: 400
+  }
+
+  article>div strong {
+    font-style: normal;
+    font-weight: 400
+  }
+
+  .cGold {
+    color: #f46e00
+  }
+
+  .cGray {
+    color: #bababa
+  }
+
+  .cGreen {
+    color: #dc2e2e
+  }
 </style>
