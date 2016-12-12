bdb22b96aee827ca64ab883cf14ef069c50b0c7b
wzw <1007442838@qq.com>
2016-12-12 17:23:40 +0800

    王志伟 让main和app中的路由钩子调用统一函数,进行权限判断 main增加对storage是否可用的判定 封装layer.load 和layer.confirm方法

--- src/js/app.js
+++ src/js/app.js
@@ -13,18 +13,7 @@ export default {
   },
   watch:{
     $route(to,from){
-      // console.log("路由监听");
-      var meta = to.matched[0].meta
-      var state = this.$store.state
-      if(meta.user){
-        // console.log("需要登录")
-        if (!state.UserName) {
-          this.$router.push("/login")
-        }else if(state.agent){
-          // console.log("需要代理")
-          state.AgentRebate||this.$router.push("/notfount")
-        }
-      }
+      RootApp.beforEnter(to)
     }
   }
 }
