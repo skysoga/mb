061bb8fbef3e2748c8418d19a3d041ce7d5d623f
virjay <1007470977@qq.com>
2016-12-16 14:32:25 +0800

    快3页面加上内容

--- src/views/lottery_k3.vue
+++ src/views/lottery_k3.vue
@@ -1,41 +1,196 @@
 <template>
  <div>
-    <header class="top">
+<header class="top">
     <div>
-      <p>玩法</p>
-      <span id="MethodStr">
-        和值
-        <i class="iconfont xiala"></i>
-      </span>
+      <p>玩法</p><span id="MethodStr">和值<i class="iconfont xiala"></i></span>
     </div>
-    <div class="fr" id="lotteryChoice">甘肃<i class="iconfont xiala"></i></div>
+    <div class="fr" id="lotteryChoice">江苏<i class="iconfont xiala"></i><div class="fix" style="display: none;"><a class="active" href="/lottery_k3.html?lottery=1401">江苏快3</a><a class="active" href="/lottery_k3.html?lottery=1402">安徽快3</a><a class="active" href="/lottery_k3.html?lottery=1403">广西快3</a><a class="active" href="/lottery_k3.html?lottery=1404">吉林快3</a><a class="active" href="/lottery_k3.html?lottery=1405">湖北快3</a><a class="active" href="/lottery_k3.html?lottery=1406">北京快3</a><a class="active" href="/lottery_k3.html?lottery=1407">大发快3</a><a class="active" href="/lottery_k3.html?lottery=1408">河北快3</a><a class="active" href="/lottery_k3.html?lottery=1409">贵州快3</a><a class="active" href="/lottery_k3.html?lottery=1410">上海快3</a><a class="active" href="/lottery_k3.html?lottery=1411">甘肃快3</a></div></div>
+    <a class="iconfont" href="/index.html"></a>
+  <ul class="betTopDetail" style="display: none;"><li class="checked">
+        <p class="p0">和值</p>
+        <p class="p1">1.946-188.56倍</p>
+        <p class="p2">
+          <span class="Dice Dice1"></span>
+          <span class="Dice Dice2"></span>
+          <span class="Dice Dice3"></span>
+        </p>
+      </li><li>
+        <p class="p0">三同号通选</p>
+        <p class="p1">赔率31.42倍</p>
+        <p class="p2">
+          <span class="Dice Dice1"></span>
+          <span class="Dice Dice1"></span>
+          <span class="Dice Dice1"></span>
+        </p>
+      </li><li>
+        <p class="p0">三同号单选</p>
+        <p class="p1">赔率188.56倍</p>
+        <p class="p2">
+          <span class="Dice Dice1"></span>
+          <span class="Dice Dice1"></span>
+          <span class="Dice Dice1"></span>
+        </p>
+      </li><li>
+        <p class="p0">三不同号</p>
+        <p class="p1">赔率31.42倍</p>
+        <p class="p2">
+          <span class="Dice Dice2"></span>
+          <span class="Dice Dice3"></span>
+          <span class="Dice Dice5"></span>
+        </p>
+      </li><li>
+        <p class="p0">三连号通选</p>
+        <p class="p1">赔率7.85倍</p>
+        <p class="p2">
+          <span class="Dice Dice1"></span>
+          <span class="Dice Dice2"></span>
+          <span class="Dice Dice3"></span>
+        </p>
+      </li><li>
+        <p class="p0">二同号复选</p>
+        <p class="p1">赔率12.57倍</p>
+        <p class="p2">
+          <span class="Dice Dice1"></span>
+          <span class="Dice Dice1"></span>
+          <span class="Dice Dice3"></span>
+        </p>
+      </li><li>
+        <p class="p0">二同号单选</p>
+        <p class="p1">赔率62.85倍</p>
+        <p class="p2">
+          <span class="Dice Dice1"></span>
+          <span class="Dice Dice1"></span>
+          <span class="Dice Dice3"></span>
+        </p>
+      </li><li>
+        <p class="p0">二不同号</p>
+        <p class="p1">赔率6.28倍</p>
+        <p class="p2">
+          <span class="Dice Dice1"></span>
+          <span class="Dice Dice4"></span>
+          <span class="Dice Dice4"></span>
+        </p>
+      </li><li></li></ul></header>
 
-    <a class="iconfont" @click = "back2index">&#xe60a;</a>
-  </header>
-
-  <section class="State">
+<section class="State">
     <div>
-      <p id="openIssuse"><!-- 201607011234期开奖中 --></p>
-      <div id="openResult">
-        <div class="Dice"></div>
-        <div class="Dice"></div>
-        <div class="Dice"></div>
+      <p id="openIssuse">1216036期开奖号码</p>
+      <div id="openResult" style="display: block;">
+        <div class="Dice Dice1"></div>
+        <div class="Dice Dice3"></div>
+        <div class="Dice Dice4"></div>
         <i class="iconfont xiala"></i>
       </div>
     </div>
     <div>
-      <p id="thisIssuse"><!-- 距201607011234期截止 --></p>
-      <div id="time"></div>
+      <p id="thisIssuse">1216037期投注截止</p>
+      <div id="time">05:42</div>
     </div>
-    <table>
-      <!-- 这里放开奖结果 -->
-    </table>
+    <table style="display: none;"><tr> <td>期号</td> <td>开奖号码</td> <td>和值</td> <td>大小</td> <td>单双</td> </tr><tr>
+          <td>&nbsp;&nbsp;&nbsp;1216036<i>—</i></td>
+          <td>
+            <div class="Dice Dice1"></div>
+            <div class="Dice Dice3"></div>
+            <div class="Dice Dice4"></div>
+          </td>
+          <td>8</td>
+          <td><span class="light small">小</span></td>
+          <td><span class="light double">双</span></td>
+        </tr><tr>
+          <td>&nbsp;&nbsp;&nbsp;1216035<i>—</i></td>
+          <td>
+            <div class="Dice Dice2"></div>
+            <div class="Dice Dice5"></div>
+            <div class="Dice Dice6"></div>
+          </td>
+          <td>13</td>
+          <td><span class="light big">大</span></td>
+          <td><span class="light single">单</span></td>
+        </tr><tr>
+          <td>&nbsp;&nbsp;&nbsp;1216034<i>—</i></td>
+          <td>
+            <div class="Dice Dice2"></div>
+            <div class="Dice Dice3"></div>
+            <div class="Dice Dice6"></div>
+          </td>
+          <td>11</td>
+          <td><span class="light big">大</span></td>
+          <td><span class="light single">单</span></td>
+        </tr><tr>
+          <td>&nbsp;&nbsp;&nbsp;1216033<i>—</i></td>
+          <td>
+            <div class="Dice Dice1"></div>
+            <div class="Dice Dice2"></div>
+            <div class="Dice Dice3"></div>
+          </td>
+          <td>6</td>
+          <td><span class="light small">小</span></td>
+          <td><span class="light double">双</span></td>
+        </tr><tr>
+          <td>&nbsp;&nbsp;&nbsp;1216032<i>—</i></td>
+          <td>
+            <div class="Dice Dice3"></div>
+            <div class="Dice Dice5"></div>
+            <div class="Dice Dice6"></div>
+          </td>
+          <td>14</td>
+          <td><span class="light big">大</span></td>
+          <td><span class="light double">双</span></td>
+        </tr><tr>
+          <td>&nbsp;&nbsp;&nbsp;1216031<i>—</i></td>
+          <td>
+            <div class="Dice Dice4"></div>
+            <div class="Dice Dice4"></div>
+            <div class="Dice Dice5"></div>
+          </td>
+          <td>13</td>
+          <td><span class="light big">大</span></td>
+          <td><span class="light single">单</span></td>
+        </tr><tr>
+          <td>&nbsp;&nbsp;&nbsp;1216030<i>—</i></td>
+          <td>
+            <div class="Dice Dice3"></div>
+            <div class="Dice Dice4"></div>
+            <div class="Dice Dice4"></div>
+          </td>
+          <td>11</td>
+          <td><span class="light big">大</span></td>
+          <td><span class="light single">单</span></td>
+        </tr><tr>
+          <td>&nbsp;&nbsp;&nbsp;1216029<i>—</i></td>
+          <td>
+            <div class="Dice Dice1"></div>
+            <div class="Dice Dice4"></div>
+            <div class="Dice Dice6"></div>
+          </td>
+          <td>11</td>
+          <td><span class="light big">大</span></td>
+          <td><span class="light single">单</span></td>
+        </tr><tr>
+          <td>&nbsp;&nbsp;&nbsp;1216028<i>—</i></td>
+          <td>
+            <div class="Dice Dice2"></div>
+            <div class="Dice Dice3"></div>
+            <div class="Dice Dice3"></div>
+          </td>
+          <td>8</td>
+          <td><span class="light small">小</span></td>
+          <td><span class="light double">双</span></td>
+        </tr><tr>
+          <td>&nbsp;&nbsp;&nbsp;1216027<i>—</i></td>
+          <td>
+            <div class="Dice Dice1"></div>
+            <div class="Dice Dice5"></div>
+            <div class="Dice Dice6"></div>
+          </td>
+          <td>12</td>
+          <td><span class="light big">大</span></td>
+          <td><span class="light double">双</span></td>
+        </tr></table>
   </section>
-  <div class="main">
+<div class="main">
     <div class="MethodWrap">
-      <div class="Method">
-        <!-- 这里放选注主体 -->
-      </div>
+      <div class="Method Method0"><p class="MethodMsg">猜3个开奖号相加的和，3-10为小，11-18为大。</p><ul><li>大<p>赔率1.946</p></li><li>小<p>赔率1.946</p></li><li>单<p>赔率1.946</p></li><li>双<p>赔率1.946</p></li><li>3<p>赔188.56</p></li><li>4<p>赔62.85</p></li><li>5<p>赔31.42</p></li><li>6<p>赔18.85</p></li><li>7<p>赔12.57</p></li><li>8<p>赔8.97</p></li><li>9<p>赔7.54</p></li><li>10<p>赔6.98</p></li><li>11<p>赔6.98</p></li><li>12<p>赔7.54</p></li><li>13<p>赔8.97</p></li><li>14<p>赔12.57</p></li><li>15<p>赔18.85</p></li><li>16<p>赔31.42</p></li><li>17<p>赔62.85</p></li><li>18<p>赔188.56</p></li></ul></div>
     </div>
   </div>
 
