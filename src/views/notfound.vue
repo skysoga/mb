6fda4cc6a18e7c0274158b2bed583977990a1180
明明明天 <1007503699@qq.com>
2016-12-23 18:04:48 +0800

    修改a改为router-link

--- src/views/notfound.vue
+++ src/views/notfound.vue
@@ -3,9 +3,9 @@
 		<div class="notFound">
 			<img src="http://images.dafa176.com/system/pc/error/404.png" alt="" width="280" height="188">
 			<h1>抱歉，无法访问此页面！</h1>
-			<a class="errorService" href="/">返回首页</a>
+			<router-link class="errorService" to="/">返回首页</router-link>
 		</div>
-		<a class="errorLogo" href="javascript:;"><img id="Logo1" src="" alt=""></a>
+		<router-link class="errorLogo" href="javascript:;"><img id="Logo1" src="" alt=""></router-link>
 	</div>
 </template>
 <script src="../js/notfound.js"></script>
