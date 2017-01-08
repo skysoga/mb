ef7eca9c0cd51839f83dafd5565b10eedb04c2d8
lxs <1007390887@qq.com>
2017-01-08 15:39:17 +0800

    提现只有一张银行卡时提示

--- src/js/withdraw.js
+++ src/js/withdraw.js
@@ -125,6 +125,8 @@ export default{
     toggle(){
       if(this.UserBankList.length>1){
         this.isShow=!this.isShow
+      }else{
+        layer.msg("您只绑定了一张银行卡，无法切换。")
       }
     },
     setCard(key,id){
