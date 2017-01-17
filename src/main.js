00f7b20d67d4acf3053d69142172acf4867edac0
wzw <1007442838@qq.com>
2017-01-17 18:15:08 +0800

    为ActivityConfig做容错 修改pple-touch-icon

--- src/main.js
+++ src/main.js
@@ -144,10 +144,16 @@ window._fetch = function (data){
         })()
         notRes||resolve(json)
       }).catch(r=>{
+        console.log(r.message)
         FetchCatch("网络数据错误",resolve)
       })
-    }).catch((res)=>{
-      FetchCatch("网络错误，请检查网络状态",resolve)
+    }).catch((r)=>{
+      console.log(r.message);
+      if ("Failed to fetch"===r.message) {
+        FetchCatch("您的设备失去了网络连接",resolve)
+      }else{
+        FetchCatch("网络错误，请检查网络状态",resolve)
+      }
     })
   })
 }
@@ -486,7 +492,7 @@ window.RootApp={
       if (!a) {return}
       for (var i = a.length - 1; i >= 0; i--) {
         if (typeof(a[i].Img)=="object") {
-          a[i].Img=a[i].Img[0];
+          a[i].Img=a[i].Img&&a[i].Img[0];
         }
       }
     })(data.ActivityConfig)
