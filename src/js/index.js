5052cd826f9d8f377fca6d9ae2219caab38f2197
wzw <1007442838@qq.com>
2017-01-21 14:36:48 +0800

    适应性修改注册链接的跳转

--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,13 @@ export default {
     swiperSlide
   },
   beforeRouteEnter:(to, from, next) => {
+    var search=location.search
+    if(search&&(search.search(/^\?id=\d{8}$/)===0)){
+      //如果是注册
+      next(vm=>{
+        router.push("/register"+search)
+      })
+    }
     var NologApp = _App&&!state.UserName
     if (NologApp) {
       to.meta.title='未登录'
