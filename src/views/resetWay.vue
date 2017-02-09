d1372be0d5cc512a169301272b92b80ef2f65448
wzw <1007442838@qq.com>
2017-02-09 19:52:50 +0800

    app忘记密码过程不显示通过客服 线路改成app下载

--- src/views/resetWay.vue
+++ src/views/resetWay.vue
@@ -1,32 +1,42 @@
 <template>
     <div class="main">
     <div class="options innerWrap">
-      <p class="hr1px" v-if="ResetSafePwd"></p>
-      <router-link :to="{path:'verifySafePwd',query:{Q:reUrl}}" v-if="ResetSafePwd" class="active">
-        <span>通过安全密码</span>
-        <i class="fr"><span class="iconfont right"></span></i>
-      </router-link>
-      <p class="hr1px" v-if="ResetMobile"></p>
-      <router-link :to="{path:'verifyMobile',query:{Q:reUrl}}" v-if="ResetMobile" class="active">
-        <span>通过密保手机</span>
-        <i class="fr"><span class="iconfont right"></span></i>
-      </router-link>
-      <p class="hr1px" v-if="ResetQuestion"></p>
-      <router-link :to="{path:'verifyQuestion',query:{Q:reUrl}}" v-if="ResetQuestion" class="active">
-        <span>通过密保问题</span>
-        <i class="fr"><span class="iconfont right"></span></i>
-      </router-link>
-      <p class="hr1px" v-if="ResetMail"></p>
-      <router-link :to="{path:'verifyMail',query:{Q:reUrl}}" v-if="ResetMail" class="active">
-        <span>通过密保邮箱</span>
-        <i class="fr"><span class="iconfont right"></span></i>
-      </router-link>
-      <p class="hr1px"></p>
-      <router-link :to="'service'" class="active" v-show="reUrl">
-        <span>通过在线客服</span>
-        <i class="fr"><span class="iconfont right"></span></i>
-      </router-link>
-      <p class="hr1px"></p>
+      <template v-if="ResetSafePwd">
+        <p class="hr1px"></p>
+        <router-link :to="{path:'verifySafePwd',query:{Q:reUrl}}" class="active">
+          <span>通过安全密码</span>
+          <i class="fr"><span class="iconfont right"></span></i>
+        </router-link>
+      </template>
+      <template v-if="ResetMobile">
+        <p class="hr1px"></p>
+        <router-link :to="{path:'verifyMobile',query:{Q:reUrl}}"class="active">
+          <span>通过密保手机</span>
+          <i class="fr"><span class="iconfont right"></span></i>
+        </router-link>
+      </template>
+      <template v-if="ResetQuestion">
+        <p class="hr1px"></p>
+        <router-link :to="{path:'verifyQuestion',query:{Q:reUrl}}" class="active">
+          <span>通过密保问题</span>
+          <i class="fr"><span class="iconfont right"></span></i>
+        </router-link>
+      </template>
+      <template v-if="ResetMail">
+        <p class="hr1px"></p>
+        <router-link :to="{path:'verifyMail',query:{Q:reUrl}}" class="active">
+          <span>通过密保邮箱</span>
+          <i class="fr"><span class="iconfont right"></span></i>
+        </router-link>
+      </template>
+      <template v-if="$store.state.SiteConfig">
+        <p class="hr1px"></p>
+        <router-link :to="'service'" class="active" v-show="reUrl">
+          <span>通过在线客服</span>
+          <i class="fr"><span class="iconfont right"></span></i>
+        </router-link>
+      </template>
+        <p class="hr1px"></p>
     </div>
   </div>
 </template>
