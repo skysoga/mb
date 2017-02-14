07f307ea02747db8d1bd91e90e2dc40170fae87a
lxs <1007390887@qq.com>
2017-02-14 11:08:11 +0800

    手机版  注单详情 回退按钮要点击二次的问题和撤单后回列表

--- src/views/betDetail.vue
+++ src/views/betDetail.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main">
-    <div>
+    <div style="float:right;width:20%">
       <div class="textMore dataType" v-if="(res_data.State==='等待开奖')&&(UID==0)" @click='CancelBet($route.query.ID)'><em>撤单</em></div>
     </div>
     <div class="tzHead">
@@ -54,7 +54,7 @@ export default {
         yes: function() {
           _fetch({Action:"CancelBet",ID:id}).then((data)=>{
             if(data.Code===1){
-              layer.url(data.StrCode)
+              layer.url(data.StrCode,'/betCenter/betRecord')
             }else {
               layer.msgWarn(data.StrCode)
             }
