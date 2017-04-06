d64fdc145b18b407cf60c27b512cea58720cea5c
lxs <1007390887@qq.com>
2017-04-06 10:45:01 +0800

    VUE 增加PK10 机选注单

--- src/js/page_config/lt_pk10.js
+++ src/js/page_config/lt_pk10.js
@@ -1,4 +1,4 @@
-import {countSingle, betSum} from '../kit'
+import {countSingle, betSum, _random, _pk10} from '../kit'
 
 var pk10Config = {
   "定位胆":{
@@ -193,14 +193,53 @@ var pk10Play = {
   A11:{render:['first'], alg:countSingle},
 }
 
+function syx5OneStar(){
+  var line =  Math.floor(Math.random() * 10)
+  var res = []
+  for(var i = 0;i < 10;i++){
+    var lineRes = []
+    if(line === i){
+      var feed = Math.floor(Math.random() * 10)
+      lineRes.push(_pk10[feed])
+    }
+    res.push(lineRes)
+  }
+  return res
+}
+
+function getSYX5NoteRandom(num, baseArr, noNeedOrder){
+  var source = baseArr.slice()
+  console.log(source)
+  var res = []
+  for(var i = 0;i < num;i++){
+    var feed = Math.floor(Math.random() * source.length)  //随机索引
+    res.push(source[feed]) //添加到每一行的结果数组中
+    source.splice(feed, 1)     //从源数组中剔掉一个，保证单行不会出现重复
+  }
+
+  if(noNeedOrder === true){
+    res = res.sort((a,b)=>a-b)
+  }
+  console.log(res)
+  return res
+}
+
 //复式注单随机生成函数
 var pk10Random = {
-
+  F11:()=>syx5OneStar(),//定位胆
+  E11:()=>_random([1,1,1,1,1], false, _pk10),//前五
+  D11:()=>_random([1,1,1,1], false, _pk10),//前四
+  C11:()=>_random([1,1,1], false, _pk10),//前三
+  B11:()=>_random([1,1], false, _pk10),//前二
+  A11:()=>_random([1], false, _pk10)//前一
 }
 
 //单式注单随机生成函数
 var pk10RandomNote = {
-
+  E12:()=>getSYX5NoteRandom(5, _pk10),//前五单式
+  D12:()=>getSYX5NoteRandom(4, _pk10),//前四单式
+  C12:()=>getSYX5NoteRandom(3, _pk10),//前三单式
+  B12:()=>getSYX5NoteRandom(2, _pk10)//前二单式
 }
 
 //特殊的，不能够注数为1的一些玩法
