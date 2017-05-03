da533a1a6a2ff7a4005de7acba932e3926f505d7
shiang <shiang20160314@gmail.com>
2017-05-03 13:13:43 +0800

    更新移除多餘樣式

--- src/views/lotteryHall.vue
+++ src/views/lotteryHall.vue
@@ -4,22 +4,19 @@
       <h2>{{initData.Title}}</h2>
     </article> -->
     <!-- 彩种类别 -->
-    <div class="nav-slider-wrapper">
-      <div class="nav-top-pos">
-        <div class="nav-top">
-          <ul>
-            <li v-for = "item in lotteryConfig"
-                :class = "{on:item.LotteryClassName === nowLotteryClass}"
-                @click="changeNowLotteryClass(item.LotteryClassName, item.LotteryList)">
-              <a>
-                <p>{{item.LotteryClassName}}</p>
-              </a>
-            </li>
-          </ul>
-        </div>
+    <div class="nav-top-pos">
+      <div class="nav-top">
+        <ul>
+          <li v-for = "item in lotteryConfig"
+              :class = "{on:item.LotteryClassName === nowLotteryClass}"
+              @click="changeNowLotteryClass(item.LotteryClassName, item.LotteryList)">
+            <a>
+              <p>{{item.LotteryClassName}}</p>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
-
     <!-- 彩种列表  -->
     <div id="HotLottery"
          class="lottery-list-box"
@@ -32,7 +29,7 @@
             <span>{{LotteryList[l].LotteryIntro}}</span>
           </router-link>
         </li>
-        <li v-for=" l in 3 - ( nowDisplayList.length%3 || 3 )"></li>
+        <li class="add-item" v-for=" l in 3 - ( nowDisplayList.length%3 || 3 )"></li>
       </ul>
     </div>
   </div>
