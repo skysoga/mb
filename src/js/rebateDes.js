cfd1f8311e3650d1e9ccbc91d0b90e44342b2505
lxs <1007390887@qq.com>
2017-05-13 16:12:37 +0800

    返点赔率表 排列3/5 改为 排列3

--- src/js/rebateDes.js
+++ src/js/rebateDes.js
@@ -4,7 +4,7 @@ import BottomBox from '../components/bottom-box';
       return{
         BetweenType:'',
         BottomBoxShow:false,
-        BottomBoxList:{K3:"快3",SSC:"时时彩",SYX5:"11选5",FC3D:"福彩3D",PL35:"排列3/5",KL8:"北京快乐8",PK10:"北京PK10"},
+        BottomBoxList:{K3:"快3",SSC:"时时彩",SYX5:"11选5",FC3D:"福彩3D",PL35:"排列3",KL8:"北京快乐8",PK10:"北京PK10"},
         AgentRebate:'',//自身返点数组
         setObj:{Max:'8.0',Min:'0.0'},//最大返点
         ArrObj:'',//当前总数据
@@ -46,7 +46,7 @@ import BottomBox from '../components/bottom-box';
       },
       setMsg(key,index){
         if(index>0){
-          return key=="SSC"?"奖金":"赔率"
+          return key!="K3"?"奖金":"赔率"
         }else{
           return ""
         }
