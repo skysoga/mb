dc181e567167da33880b816bab486d76a2519c3d
rouwan <525087840@qq.com>
2017-06-29 09:45:41 +0800

    路由名字变更

---
+++ src/routes/lottery.js
@@ -0,0 +1,73 @@
+//广营负责页面的路由
+const register =require('../views/register')
+const rechargeWay =require('../views/rechargeWay')
+const normalPay =require('../views/normalPay')
+const quickPay =require('../views/quickPay')
+const lotteryHall =require('../views/lotteryHall')
+const lotteryPage = require('../views/lotteryPage')
+// const lottery_common = require('../views/lottery_common')
+// const lottery_k3 = require('../views/lottery_k3')
+// const lottery_syx5 = require('../views/lottery_syx5')
+
+var routes = [
+  {
+    path: '/register',
+    name: '注册',
+    meta:{
+      title:"用户注册",
+      nav:false,
+      link:"/index",
+      service:true,
+      user:false,
+      agent:false
+    },
+    component: register
+  },{
+    path: '/rechargeWay',
+    name: '选择充值方式',
+    meta:{
+      title:"选择充值方式",
+      link:"/userCenter",
+      user:1,
+    },
+    component: rechargeWay
+  },{
+    path: '/normalPay',
+    name: '普通充值',
+    meta: {
+      title: '',
+      link: '/rechargeWay',
+      user: true,
+    },
+    component: normalPay
+  },{
+    path: '/quickPay',
+    name: '快捷充值',
+    meta: {
+      title: '',
+      link: '/rechargeWay',
+      user: true
+    },
+    component: quickPay
+  },{
+    path: '/lotteryHall',
+    name: '全部彩种',
+    meta:{
+      title:"全部彩种",
+      nav:0,
+      link:'index'
+    },
+    component: lotteryHall
+  },{
+    path: '/lottery/:type/:code',
+    name: '彩种',
+    meta:{
+      // title:"彩种",
+      user: true,
+      component:lotteryPage
+    },
+    component: lotteryPage
+  }
+]
+
+module.exports = routes
