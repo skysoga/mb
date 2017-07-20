985e385d430de09a512f659dff120453a831f831
lxs <1007390887@qq.com>
2017-07-20 11:39:14 +0800

    下级开户排列3/5 改为 排列3

--- src/js/manageInvite.js
+++ src/js/manageInvite.js
@@ -1,7 +1,7 @@
 export default {
   data:()=>{
     return{
-      caiName:{SSC:"时时彩",XYNC:"幸运农场",PK10:"北京PK10",KL8:"北京快乐8",PL35:"排列3/5",FC3D:"福彩3D",SYX5:"11选5",K3:"快3","6HC":"六合彩"},
+      caiName:{SSC:"时时彩",XYNC:"幸运农场",PK10:"北京PK10",KL8:"北京快乐8",PL35:"排列3",FC3D:"福彩3D",SYX5:"11选5",K3:"快3","6HC":"六合彩"},
       UserType:1,
       caiList:'',
       dval:''
