ed56bdc6c8d7ba7691fe9a11e98df3d32acddace
goudan <goudan@gmail.com>
2017-09-15 13:43:15 +0800

    新快3页面更新

--- src/views/newk3/main.vue
+++ src/views/newk3/main.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="newContainer">
+  <div class="newContainer" @touchend="touchend">
     <div class="betContainer">
       <div class="video"></div>
       <mainheader></mainheader>
-      <div :style="'min-height:'+mainHight+'px'" class="betbox">
+      <div class="betbox">
         <div class="space"></div>
         <div class="topshadow"></div>
-        <div class="newmain">
+        <div ref="buttonList" class="newmain">
           <ul class="buttonList fix">
             <li :class = "{curr:chosen.indexOf('大') > -1}"><span @click="addNum('大')">大</span></li>
             <li :class = "{curr:chosen.indexOf('小') > -1}"><span @click="addNum('小')">小</span></li>
@@ -30,7 +30,7 @@
             <li :class = "{curr:chosen.indexOf('18') > -1}"><span @click="addNum('18')">18</span></li>
           </ul>
         </div>
-        <div class="bottomshadow"></div>
+        <div class="bottomshadow" :style="{height:mainHeight+'px'}"></div>
         <div class="space2"></div>
       </div>
       <mainfooter ref="footer" :betshow="betshow" :chosen="chosen"></mainfooter>
@@ -45,7 +45,8 @@
       return{
         chosen:[],
         betshow:0,
-        mainHight:0
+        mainHeight:0,
+        beforeScreenHeight:0,
       }
     },
     created(){
@@ -65,10 +66,28 @@
         }else{
           this.betshow = 0
         }
+      },
+      setHeight(){
+        var screenHeight = document.documentElement.clientHeight
+        if (this.beforeScreenHeight == screenHeight) {
+          return
+        }
+        this.beforeScreenHeight = screenHeight
+        var footerHeight = this.$refs.footer.$el.offsetHeight
+        var buttonList = this.$refs.buttonList.offsetHeight
+        this.mainHeight = screenHeight - footerHeight - (2.3+1)*em - buttonList + 1.6*em
+        console.log('screenHeight:'+screenHeight)
+        console.log('footerHeight:'+footerHeight)
+        console.log(this.mainHeight)
+      },
+      touchend(e){
+        setTimeout(()=>{
+          this.setHeight()
+        },100)
       }
     },
     mounted(){
-      this.mainHight = document.documentElement.clientHeight - this.$refs.footer.$el.offsetHeight + 1.6*em
+      this.setHeight()
     },
     components: {
       mainheader,
@@ -164,7 +183,7 @@
   .bottomshadow{
     position:absolute;
     width:100%;
-    height:3.4em;
+    min-height:2.6em;
     background:rgba(0,0,0,.6);
   }
 </style>
