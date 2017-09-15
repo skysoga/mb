d22b44f9d0cd595b019b01dbe7f980c1fbcd7e4d
lxs <1007390887@qq.com>
2017-09-15 17:11:20 +0800

    密码禁止使用& 字符

--- src/plugins/va.js
+++ src/plugins/va.js
@@ -131,7 +131,7 @@ var regList = {
   SmsCode: /^\d{4}$/,
   MailCode: /^\d{4}$/,
   UserName: /^[\w|\d]{4,}$/,  //通用的账号正则（不包括注册）
-  Password: /^[\w!@#$%^&*.]{6,16}$/,
+  Password: /^[\w!@#$%^*.]{6,16}$/,
   Mobile: /^1[3|4|5|7|8]\d{9}$/,
   RealName: /^[\u4e00-\u9fa5|·]{2,16}$|^[a-zA-Z|\s]{2,20}$/,
   BankNum: /^\d{10,19}$/,
