666101f4cb317b8844478ca12f6ac1c6c655c950
goudan <goudan@gmail.com>
2017-11-22 15:38:11 +0800

    更新飞机

--- src/views/newk3/gifts/airplane.vue
+++ src/views/newk3/gifts/airplane.vue
@@ -26,18 +26,16 @@
 	}
 }
 .airplane{
-	width: 10.6rem;
-  height: 4.3rem;
+  width: 9.6rem;
+  height: 3.9rem;
   background-size: 18rem !important;
   background-repeat: no-repeat !important;
 }
 .light{
-  width: 34rem;
-  height: 8rem;
-  background-position: 35rem 24rem !important;
-  left: 7rem;
-  top: -1.9rem;
-  background-size: 80rem !important;
+  width: 17rem;
+  height: 5rem;
+  background-position: 17rem 13rem !important;
+  background-size: 40rem !important;
 }
 .moving.gift{
 	.airplane{
@@ -45,29 +43,33 @@
 	}
 	.light{
 		animation: light 10s linear;
+    transform-origin: 100% 50%;
+    transform: scale(0) translateY(9.1rem);
+    top:6.5rem;
+    left:2rem;
 	}
 }
 @keyframes light{
 	0%{
-		transform: translate(17rem,8rem);
+    transform: scale(0) rotate(16deg);
 		opacity: 1;
 	}
 	25%{
 		opacity: 1;
 	}
 	50%{
-		transform: translate(-10rem,-1rem);
+    transform: scale(1.3) rotate(16deg);
 		opacity: 0;
 	}
 	50.001%{
-		transform: translate(-42rem,-21rem) scaleX(-1) rotate(-33deg);
+		transform: translate(-24rem,-19.8rem) scale(0) rotate(16deg);
 		opacity: 1;
 	}
 	75%{
 		opacity: 1;
 	}
 	100%{
-		transform: translate(-24rem,-17rem) scaleX(-1) rotate(-33deg);
+		transform: translate(-19.5rem,-19.6rem) scale(-1) rotate(16deg);
 		opacity: 0;
 	}
 }
@@ -80,6 +82,9 @@
 	}
 	51%{
 		transform: translate(-12rem,-13rem) scaleX(-1) rotate(-33deg);
+	}
+	75{
+		transform: translate(2rem,-2.5rem) scaleX(-1) rotate(-33deg);
 	}
 	100%{
 		transform: translate(16rem,-6rem) scaleX(-1) rotate(-33deg);
