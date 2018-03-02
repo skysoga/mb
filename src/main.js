8cc8c66403f41093bca65dff8ea7d2f27a1cc220
goudan <goudan@baidu.com>
2018-03-02 13:57:00 +0800

    取消连击拦截

--- src/main.js
+++ src/main.js
@@ -1566,14 +1566,6 @@ document.documentElement.addEventListener('touchstart', function (event) {
     event.preventDefault()
   }
 }, false)
-var lastTouchEnd = 0
-document.documentElement.addEventListener('touchend', function (event) {
-  var now = Date.now()
-  if (now - lastTouchEnd <= 300) {
-    event.preventDefault()
-  }
-  lastTouchEnd = now
-}, false)
 
 //数组随机扩展方法
 Array.prototype.shuffle = function() {
