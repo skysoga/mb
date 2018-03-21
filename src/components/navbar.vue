9845df4064ba2ddb4febaf4676c1df626e91308b
goudan <goudan@baidu.com>
2018-03-21 12:59:37 +0800

    更新文案

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -4,6 +4,6 @@
   <router-link to='/liveList'><i class="iconfont"></i><p>UU直播</p></router-link>
   <router-link to='/activity'><i class="iconfont"></i><p>活动</p></router-link>
   <router-link to='/center'><i class="iconfont"></i><p>发现</p></router-link>
-  <router-link to='/userCenter'><i class="iconfont"></i><p>我的账户</p></router-link>
+  <router-link to='/userCenter'><i class="iconfont"></i><p>我的</p></router-link>
 </nav>
 </template>
