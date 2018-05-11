ba2d6cc154e11f55216b546d43a1db2dc4d8ca9f
lxs <1007390887@qq.com>
2018-05-11 15:35:59 +0800

    修正，时时彩走势图滚动高度问题

--- src/views/lottery_common.vue
+++ src/views/lottery_common.vue
@@ -5,23 +5,25 @@
         <!-- 头部： 玩法选择， 同类彩种选择-->
 
         <lt-header></lt-header>
-        <!-- 开奖号码 以及 投注截止时间  -->
-        <div class="isLotteryCon" v-show="ifShowType">
-          <!-- 开奖结果和历史开奖结果 -->
-          <lt-result></lt-result>
-          <!-- 倒计时和我的投注 -->
-          <lt-timebar></lt-timebar>
-        </div>
-        <!-- PK10 KL8 开奖号码栏 -->
-        <lt-PKKL v-show="!ifShowType"/>
+        <template v-if="!$store.state.ShowTrendchart">
+          <!-- 开奖号码 以及 投注截止时间  -->
+          <div class="isLotteryCon" v-show="ifShowType">
+            <!-- 开奖结果和历史开奖结果 -->
+            <lt-result></lt-result>
+            <!-- 倒计时和我的投注 -->
+            <lt-timebar></lt-timebar>
+          </div>
+          <!-- PK10 KL8 开奖号码栏 -->
+          <lt-PKKL v-show="!ifShowType"/>
 
-        <!-- 投注区，各彩种不同 -->
-        <!-- <ssc></ssc> -->
-        <playArea></playArea>
+          <!-- 投注区，各彩种不同 -->
+          <!-- <ssc></ssc> -->
+          <playArea></playArea>
 
-        <!-- 倍和单位， 确认投注， 号码篮 -->
-        <ltFooterBJSC v-if="gyhlh"></ltFooterBJSC>
-        <lt-footer v-else></lt-footer>
+          <!-- 倍和单位， 确认投注， 号码篮 -->
+          <ltFooterBJSC v-if="gyhlh"></ltFooterBJSC>
+          <lt-footer v-else></lt-footer>
+        </template>
       </div>
     </div>
 
