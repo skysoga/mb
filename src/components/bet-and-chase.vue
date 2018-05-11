3068a72a57907dfea9d244a56d879f4bf8a6990f
goudan <goudan@baidu.com>
2018-05-11 15:44:08 +0800

    更新我的投注

--- src/components/bet-and-chase.vue
+++ src/components/bet-and-chase.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="b-a-c">
-    <div class="b-a-c-top">
+  <div class="b-a-c" @click.stop="">
+    <div class="b-a-c-top fix">
       <a @click="toggleBetRecord" class="back"><em class="iconfont">&#xe60a;</em></a>
+      <div class="mybet curr"><em>我的投注</em></div>
+      <div class="mychase"><em>我的追号</em></div>
+    </div>
+    <div class="betboxlist">
+      <betbox v-for="d in 5"></betbox>
+      <div class="msg noMore">更多记录请到"<a href="/userCenter" class="" id="account">我的账户</a>"查看</div>
     </div>
   </div>
 </template>
 <script>
+import betbox from '../components/betbox';
 export default{
+  components:{
+    betbox
+  },
   methods:{
     toggleBetRecord(){
       this.$store.state.lt.box === 'BetRecord' ?
@@ -23,8 +33,23 @@ export default{
 }
 .b-a-c-top{
   height: 2.3em;
-  background: rgba(250,250,250,1);
+  // background: rgba(250,250,250,1);
   position: relative;
+  border-bottom: 1px solid #d7d6d6;
+  > div{
+    height: 100%;
+    width: 50%;
+    float: left;
+    text-align: center;
+    line-height: 2.3em;
+    em{
+      font-size: .8em;
+    }
+  }
+  .curr{
+    color: #ee3945;
+    border-bottom: 2px solid;
+  }
 }
 .back{
   position: absolute;
@@ -34,5 +59,20 @@ export default{
   display: flex;
   align-items:center;
   justify-content: center;
+}
+.betboxlist{
+  padding: 10px 0;
+}
+.msg {
+  height: 2.5em;
+  text-align: center;
+  font-size: .7em;
+  padding: 1.9em 0;
+  color: #4c4c4c;
+  a{
+    display: inline;
+    padding: 0;
+    color: #ee3945;
+  }
 }
 </style>
---
+++ src/components/betbox.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="betbox">
+    <a class="active">
+      <div>
+        <p>北京快乐8<span>￥210.00</span></p><span>887109 期</span></div>
+        <div class="fr">
+          <strong class="InMoney fr">+14.950</strong>
+          <span class="InMoney fr">已中奖</span>
+        </div>
+    </a>
+    <div class="hr1px hr1px-list"></div>
+  </div>
+</template>
+<style lang="scss">
+.betbox{
+  a{
+    display: block;
+    width: 100%;
+    padding: 4px 5%;
+    color: #000;
+    >div{
+      line-height: 1.25em;
+      display: inline-block;
+      p{
+        font-size: .7em;
+        span{
+          font-size: .8em;
+          margin-left: .4em;
+        }
+      }
+      span{
+        font-size: .7em;
+        clear: both;
+        color: #989898;
+      }
+    }
+    >strong {
+      line-height: 3.8em;
+    }
+    strong{
+      font-weight: 400;
+      font-size: .7em;
+      float: right;
+    }
+  }
+}
+</style>
